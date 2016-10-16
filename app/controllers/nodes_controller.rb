bff920ebfe423f255173fe1c0975f545cee4e0aa
rodrigomoya <rodrigo@rizoma.io>
2016-10-16 20:42:28 -0300

    dlight host

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -2,7 +2,7 @@ class NodesController < ApplicationController
 
 	def index
 		begin 
-			query = RestClient.get "http://wio.rizoma.io/v1/nodes/list", {:Authorization => " token #{session[:tesape_user_token]}"}		
+			query = RestClient.get "http://dlight.io/v1/nodes/list", {:Authorization => " token #{session[:tesape_user_token]}"}		
 			@nodes = JSON.parse(query.body)['nodes']
 		end
 	end
